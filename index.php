4538fc1e9bc7a4c290e6157667a786eaba364c03
Natchanon <oopoomoo976@gmail.com>
2024-01-22 09:28:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1> Somkha  </h1>
+ <?php
+    echo"hello <br/>";
+    echo"Natchanon Sirikhamhom <br/>";
+    echo date("Y/m/d");
+ ?>
+ <?php
+ $year = 2024;
+ echo"<h1> Happy new year",$year."</h1>"
+ ?> 
+ 
+    
+</body>
+</html>
